705f5765411a2b530a2aac6a5b1f6349f3aee0f5
Andrew Sunada <asunada@mozilla.com>
2019-08-21 16:09:30 -0700

    chore: more boilerplate stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,19 @@ module.exports = {
   root: true,
 
   env: {
-    node: true
+    node: true,
   },
 
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
 
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
 
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
   },
 
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/prettier',
-    '@vue/typescript'
-  ]
+  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
 };
--- App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>Hello!</h1>
-</template>
-<style lang="scss">
-$color: red;
-h1 {
-  color: $color;
-}
-</style>
